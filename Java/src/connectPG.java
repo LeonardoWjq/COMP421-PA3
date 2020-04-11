4303f6a4c09314e9114bc02b47de6cb664c57ce0
LeonardoWjq <43143815+LeonardoWjq@users.noreply.github.com>
2020-04-11 11:33:51 -0400

    Add files via upload

--- Java/src/connectPG.java
+++ Java/src/connectPG.java
@@ -4,77 +4,120 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class connectPG {
-    private final String url = "jdbc:postgresql://comp421.cs.mcgill.ca:5432/cs421";
-    private final String user = "cs421g09";
-    private final String password = "comp4212009";
-    public Connection connect(){
-        Connection con = null;
+    private static final String url = "jdbc:postgresql://comp421.cs.mcgill.ca:5432/cs421";
+    private static final String user = "cs421g09";
+    private static final String password = "comp4212009";
+    private static Connection con; //global connection object
+    private static Statement stmt;//global statement object
+    public static boolean start(){//set up a connection and initialize the fields
         try {
             con = DriverManager.getConnection(url, user, password);
+            stmt = con.createStatement();
             System.out.println("Connected to the PostgreSQL server successfully.");
+            return true;
         } catch (SQLException e) {
             System.out.println(e.getMessage());
+            return false;
         }
-        return con;
     }
+    //print greeting statements at the main page
     public static void greeting(){
         System.out.println("Welcome to use the official Cineplex online system.");
+        System.out.println("Please choose your role:");
+        System.out.println("1 Administrator");
+        System.out.println("2 Customer");
+        System.out.println("3 Exit");
     }
-    public static void printOption(){
+    //print operation options for administrators
+    public static void printOptionAdministrator(){
         System.out.println("Please choose from the following options:");
-        System.out.println("Option1");
-        System.out.println("Option2");
-        System.out.println("Option3");
-        System.out.println("Option4");
-        System.out.println("Option5");
+        System.out.println("1 Create New Show Schedule");
+        System.out.println("2 Sale Statistics");
+        System.out.println("3 Review Duty");
+    }
+    //print operation options for customers
+    public static void printOptionCustomer(){
+        System.out.println("Please choose from the following options:");
+        System.out.println("1 Book a Ticket");
+        System.out.println("2 Recent Shows");
+        System.out.println("3 View Your Transaction Records");
     }
 
+    //main loop
     public static void main(String args []) throws SQLException {
+    	
         try {
-            DriverManager.registerDriver(new org.postgresql.Driver());
+            DriverManager.registerDriver(new org.postgresql.Driver());//register driver
             System.out.println("Found driver.");
-            connectPG connection = new connectPG();
-            Connection con = connection.connect();
-            if(con==null){
+            
+            if(!start()){//if failed to establish connection then exit the program with an error message
                 System.out.println("Failed to connect to the database.");
                 System.exit(1);
             }
-            greeting();
-            Scanner scanner = new Scanner(System.in);
-            while (true){
-                printOption();
-                String input;
-                try{
-                    System.out.print(">> ");
-                    input = scanner.nextLine();
-                }catch(InputMismatchException e){
-                    System.out.println("Please enter a valid option.");
-                    continue;
-                }
-                switch(input){
-                    case "Option1":
-                        System.out.println("option 1");
-                        break;
-                    case "Option2":
-                        System.out.println("option 2");
-                        break;
-                    case "Quit":
-                        con.close();
-                        return;
-                    default:
-                        System.out.println("Please enter a valid option.");
-                }
+            
+            Scanner scanner = new Scanner(System.in);//create a new scanner for user inputs
+            while(true){
+            	greeting();//print greeting
+            	int option;
+				try {
+					System.out.print(">> ");
+					option = scanner.nextInt();
+				} catch (InputMismatchException e) {
+					System.out.println("Please enter a valid integer as your option.");
+					continue;
+				}
+				if(option==1) {//administrator branch
+					while (true) {
+						// Authenticate Admin
+						
+						//
+						printOptionAdministrator();
+						int option_adm = scanner.nextInt();
+						switch (option_adm) {
+							case 1:
+								break;
+							case 2:
+								break;
+							case 3:
+								break;
+							default:
+							{
+								System.out.println("Please enter a valid integer in the range 1-3 as your option.");
+								break;
+							}
+						}
+					}
+				}else if(option==2) {//customer branch
+					while (true) {
+						printOptionCustomer();
+						int option_cust = scanner.nextInt();
+						switch (option_cust) {
+							case 1:
+								break;
+							case 2:
+								break;
+							case 3:
+								break;
+							default:
+							{
+								System.out.println("Please enter a valid integer in the range 1-3 as your option.");
+								break;
+							}
+						}
+					}
+				} else if (option == 3){// exit program
+					stmt.close();
+					con.close();
+					scanner.close();
+					System.out.println("Program exit successfully. Hope to see you again.");
+					System.exit(0);//exit with success code
+				} else {
+					System.out.println("Please enter a valid integer in the range 1-3 as your option.");
+				}
             }
         } catch (Exception cnfe) {
-            System.out.println("Class not found");
+            System.out.println("Class not found");//catch exception for driver
             System.exit(2);
         }
-
-
-//        Connection con = DriverManager.getConnection(url, "cs421g09", "comp4212009");
-//        System.out.println("Connect to PG");
-//        Statement statement = con.createStatement();
-
-
     }
 }
